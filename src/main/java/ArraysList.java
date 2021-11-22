79aedbab02f995af1374ae5d4068ff3280a87dde
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2021-11-22 22:05:07 +0100

    RemoveAt and IndexOf methods in ArrayList

--- src/main/java/ArraysList.java
+++ src/main/java/ArraysList.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
-/*
+/**
 Creates a ArrayList, which can
 insert, delete, search numbers.
  */
 public class ArraysList {
     private int[] numbers;
     private int count;
-    /*
+    /**
     Creates a instance of ArraysList
     with tha given limit.
     @param limit capacity of the ArrayList.
@@ -17,7 +17,7 @@ public class ArraysList {
         this.count = 0 ;
     }
 
-    /*
+    /**
     Inserts a new value to the end of the array.
     If the array is full,
     increases its capacity and
@@ -25,7 +25,7 @@ public class ArraysList {
      */
     public void insertNewValue(int value){
         if(numbers.length == count){
-            int[] newNumbers = new int[count + 1];
+            int[] newNumbers = new int[count*2];
             for(int i = 0; i < count; i++){
                 newNumbers[i] = numbers[i];
             }
@@ -33,6 +33,38 @@ public class ArraysList {
         }
         numbers[count] = value;
         count++;
+    }
+
+    /**
+     * Removes the value at the given index
+     * @param index index of the value to be removed.
+     */
+    public void removeAt(int index){
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException("Given index is invalid");
+
+        for(int i = index; i < count; i++)
+            numbers[i] = numbers[i+1];
+
+        count--;
+    }
+
+    /**
+     * Finds the index of the given value
+     * @param value value to find index of
+     */
+    public void indexOf(int value) {
+        boolean isFound = false;
+        for(int i=0; i< count ; i++){
+            if (numbers[i] == value && !isFound) {
+                isFound = true;
+                System.out.println("The index of the given value is " + i);
+            }
+        }
+        if(!isFound){
+            System.out.println("Didnt find the value !");
+        }
+
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         ArraysList arrays = new ArraysList(2);
+
+        //Inserting new values to the arraylist
         arrays.insertNewValue(10);
         arrays.insertNewValue(20);
         arrays.insertNewValue(30);
         arrays.insertNewValue(30);
+        arrays.insertNewValue(40);
         System.out.println(arrays.toString());
+
+        //Removing a value at the given index and rearranging the array.
+        try{
+            arrays.removeAt(0);
+            System.out.println("Removed successfully !");
+            System.out.println("After removing ....");
+            System.out.println(arrays.toString());
+        }catch (IllegalArgumentException iae){
+            System.out.println(iae.getMessage());
+        }
+
+        //Finding index of a given value.
+        arrays.indexOf(200);
+
     }
 }

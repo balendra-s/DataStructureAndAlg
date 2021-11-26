1229898735fccc3b7aa77fdae05b43d21a529b7f
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2021-11-26 11:53:18 +0100

    Added bubble and selection sort to the Sort class

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+H
+src/main/java/Tries.java,a/6/a6410d7f176e397fec4b519b8924bc4661c0f721
+L
+src/main/java/NodeTries.java,b/c/bc5d23bd9a53dbe61e88b1b1bd7b3ab631b6463f
+H
+src/main/java/Trees.java,a/8/a825a9a02cee03b86b884a2e69324f208fea9b30
+M
+src/main/java/ArraysList.java,0/2/02c41cbf0614e7ddfba869bcfbba6bbae0394f86
+G
+src/main/java/Main.java,e/4/e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
---
+++ src/main/java/Sorting.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Sorting {
+
+    private static void selectionSort(int[] numbers){
+        for(int i=0;i<numbers.length;i++){
+            int minIndex = i;
+            for(int j=i;j<numbers.length;j++){
+                if(numbers[minIndex] > numbers[j]){
+                    int temp = numbers[minIndex];
+                    numbers[minIndex] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+    }
+    private static void binarySort(int[] numbers) {
+        boolean isSorted;
+        for (int i = 0; i < numbers.length; i++) {
+            isSorted = true;
+            for (int j = 1; j < numbers.length - i ; j++) {
+                if (numbers[j] < numbers[j-1]) {
+                    int temp = numbers[j-1];
+                    numbers[j-1] = numbers[j];
+                    numbers[j] = temp;
+                    isSorted = false;
+                }
+                }
+            if(isSorted){
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 1,2};
+        Sorting.selectionSort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+
+
+}
